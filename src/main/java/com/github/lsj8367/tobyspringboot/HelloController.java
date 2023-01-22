4a50830a1bcbe1664747de18a1d5f2d4ab754053
lsj8367 <lsj8367@naver.com>
2023-01-22 17:31:11 +0900

    :sparkles: service layer 구현

--- src/main/java/com/github/lsj8367/tobyspringboot/HelloController.java
+++ src/main/java/com/github/lsj8367/tobyspringboot/HelloController.java
@@ -1,9 +1,12 @@
 package com.github.lsj8367.tobyspringboot;
 
+import java.util.Objects;
+
 public class HelloController {
 
     public String hello(final String name) {
-        return "Hello " + name;
+        SimpleHelloService service = new SimpleHelloService();
+        return service.sayHello(Objects.requireNonNull(name));
     }
 
 }
---
+++ src/main/java/com/github/lsj8367/tobyspringboot/SimpleHelloService.java
@@ -0,0 +1,9 @@
+package com.github.lsj8367.tobyspringboot;
+
+public class SimpleHelloService {
+
+    public String sayHello(final String name) {
+        return "Hello " + name;
+    }
+
+}
