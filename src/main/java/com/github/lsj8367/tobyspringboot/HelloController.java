c243ee23653918a4d9fb31d061a551a469286406
lsj8367 <lsj8367@naver.com>
2023-01-22 13:19:05 +0900

    Initial commit

---
+++ src/main/java/com/github/lsj8367/tobyspringboot/HelloController.java
@@ -0,0 +1,2 @@
+package com.github.lsj8367.tobyspringboot;public class HelloController {
+}
---
+++ src/test/java/com/github/lsj8367/tobyspringboot/TobyspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lsj8367.tobyspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobyspringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
