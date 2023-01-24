35c8288fc36ed80f406f3ef9f473b64d5497ede4
lsj8367 <lsj8367@naver.com>
2023-01-24 14:08:32 +0900

    :sparkles: spring java bean설정으로 변경

--- src/main/java/com/github/lsj8367/tobyspringboot/SimpleHelloService.java
+++ src/main/java/com/github/lsj8367/tobyspringboot/SimpleHelloService.java
@@ -1,5 +1,8 @@
 package com.github.lsj8367.tobyspringboot;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class SimpleHelloService implements HelloService {
 
     @Override
--- src/main/java/com/github/lsj8367/tobyspringboot/TobyspringbootApplication.java
+++ src/main/java/com/github/lsj8367/tobyspringboot/TobyspringbootApplication.java
@@ -3,25 +3,34 @@ package com.github.lsj8367.tobyspringboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+@ComponentScan(basePackages = "com.github.lsj8367.tobyspringboot")
 public class TobyspringbootApplication {
 
     public static void main(String[] args) {
-        final GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
-        applicationContext.registerBean(HelloController.class);
-        applicationContext.registerBean(SimpleHelloService.class);
-        applicationContext.refresh(); // 이 메소드를 사용하면 Java 기반 구성, XML 파일, 속성 파일,
-        // 관계형 데이터베이스 스키마 또는 일부 다른 형식일 수 있는 구성의 영구 표현을 로드하거나 새로 고친다.
+        final AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
 
-        final ServletWebServerFactory webServerFactory = new TomcatServletWebServerFactory();
-        final WebServer webServer = webServerFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("dispatcherServlet",
-                    new DispatcherServlet(applicationContext)
-                ).addMapping("/*");
-        });
-        webServer.start();
+                final ServletWebServerFactory webServerFactory = new TomcatServletWebServerFactory();
+                final WebServer webServer = webServerFactory.getWebServer(servletContext -> {
+                    servletContext.addServlet("dispatcherServlet",
+                        new DispatcherServlet(this)
+                    ).addMapping("/*");
+                });
+                webServer.start();
+
+            }
+        };
+        applicationContext.register(TobyspringbootApplication.class);
+        applicationContext.refresh();
     }
 
 }
