72486198e14c14983f7f9c13908a6f276e63f868
lsj8367 <lsj8367@naver.com>
2023-01-27 00:51:15 +0900

    :sparkles: api test 추가

---
+++ src/test/java/com/github/lsj8367/tobyspringboot/HelloApiTest.java
@@ -0,0 +1,30 @@
+package com.github.lsj8367.tobyspringboot;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+class HelloApiTest {
+
+    @Test
+    void helloApi() {
+        // http localhost:8080/hello?name=Spring
+        TestRestTemplate rest = new TestRestTemplate();
+
+        final ResponseEntity<String> res =
+            rest.getForEntity("http://localhost:8080/hello?name={name}", String.class, "Spring");
+
+        // status 200
+        // header (content-type) text/plain
+        // body Hello Spring
+
+        assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(res.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
+        assertThat(res.getBody()).isEqualTo("Hello Spring");
+    }
+}
