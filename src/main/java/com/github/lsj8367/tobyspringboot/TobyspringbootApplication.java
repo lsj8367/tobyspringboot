710e3628cc79a817b10c88de490afcb5ea2a032f
lsj8367 <lsj8367@naver.com>
2023-01-27 00:51:01 +0900

    :sparkles: springboot 로 변경

--- src/main/java/com/github/lsj8367/tobyspringboot/TobyspringbootApplication.java
+++ src/main/java/com/github/lsj8367/tobyspringboot/TobyspringbootApplication.java
@@ -1,17 +1,15 @@
 package com.github.lsj8367.tobyspringboot;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
-@ComponentScan(basePackages = "com.github.lsj8367.tobyspringboot")
+@ComponentScan
 public class TobyspringbootApplication {
 
     @Bean
@@ -25,24 +23,7 @@ public class TobyspringbootApplication {
     }
 
     public static void main(String[] args) {
-        final AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-            @Override
-            protected void onRefresh() {
-                super.onRefresh();
-
-                final ServletWebServerFactory webServerFactory = this.getBean(ServletWebServerFactory.class);
-                final DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-
-                final WebServer webServer = webServerFactory.getWebServer(servletContext -> {
-                    servletContext.addServlet("dispatcherServlet", dispatcherServlet)
-                        .addMapping("/*");
-                });
-                webServer.start();
-
-            }
-        };
-        applicationContext.register(TobyspringbootApplication.class);
-        applicationContext.refresh();
+        SpringApplication.run(TobyspringbootApplication.class, args);
     }
 
 }
