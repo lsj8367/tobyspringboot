29d5d79a8cd2d2ffc2188987fa07277be675b78d
lsj8367 <lsj8367@naver.com>
2023-01-29 16:46:44 +0900

    :sparkles: 실패케이스 예외처리

--- src/test/java/com/github/lsj8367/tobyspringboot/HelloApiTest.java
+++ src/test/java/com/github/lsj8367/tobyspringboot/HelloApiTest.java
@@ -1,7 +1,7 @@
 package com.github.lsj8367.tobyspringboot;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import org.assertj.core.api.Assertions;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpHeaders;
