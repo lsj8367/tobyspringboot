51cd49d3230789d32433e27a5bc7f1551e68eb0b
lsj8367 <lsj8367@naver.com>
2023-01-31 01:15:34 +0900

    :sparkles: using meta annotation

--- src/main/java/com/github/lsj8367/tobyspringboot/TobyspringbootApplication.java
+++ src/main/java/com/github/lsj8367/tobyspringboot/TobyspringbootApplication.java
@@ -1,26 +1,10 @@
 package com.github.lsj8367.tobyspringboot;
 
+import com.github.lsj8367.tobyspringboot.helloboot.MySpringBootAnnotation;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration
-@ComponentScan
+@MySpringBootAnnotation
 public class TobyspringbootApplication {
-
-    @Bean
-    public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
-    }
-
-    @Bean
-    public DispatcherServlet dispatcherServlet() {
-        return new DispatcherServlet();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(TobyspringbootApplication.class, args);
