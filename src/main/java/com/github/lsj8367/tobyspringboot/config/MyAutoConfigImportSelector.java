ded173fedac1f0a2955e0d1da279469cd5476b44
lsj8367 <lsj8367@naver.com>
2023-02-02 00:03:58 +0900

    :sparkles: importSelector meta-inf 주입

--- src/main/java/com/github/lsj8367/tobyspringboot/config/MyAutoConfigImportSelector.java
+++ src/main/java/com/github/lsj8367/tobyspringboot/config/MyAutoConfigImportSelector.java
@@ -1,18 +1,29 @@
 package com.github.lsj8367.tobyspringboot.config;
 
+import java.util.stream.StreamSupport;
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
 
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigImportSelector(final ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
     // 패키지 path + ClassName을 통해 설정 정보들을 가져올 수 있게됨.
     // @Import 어노테이션에서 해당 selector만 넣어주게 되면 이 설정을 토대로 설정 정보들을 가져오게 된다.
     @Override
     public String[] selectImports(final AnnotationMetadata importingClassMetadata) {
-        return new String[]{
-            "com.github.lsj8367.tobyspringboot.config.autoconfig.DispatcherServletConfig",
-            "com.github.lsj8367.tobyspringboot.config.autoconfig.TomcatWebServerConfig"
-        };
+//        List<String> autoConfigs = new ArrayList<>();
+//        for (String s : ImportCandidates.load(MyAutoConfiguration.class, classLoader)) {
+//            autoConfigs.add(s);
+//        }
+//        return autoConfigs.toArray(new String[0]);
+        final Iterable<String> candidates = ImportCandidates.load(MyAutoConfiguration.class, classLoader);
+        return StreamSupport.stream(candidates.spliterator(), false).toArray(String[]::new);
     }
 
 }
