df297688461ec597368eed17f52f6cf7b06d7ca4
lsj8367 <lsj8367@naver.com>
2023-02-04 12:29:05 +0900

    :sparkles: jetty 구성 추가

--- src/main/java/com/github/lsj8367/tobyspringboot/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/com/github/lsj8367/tobyspringboot/config/autoconfig/JettyWebServerConfig.java
@@ -1,16 +1,15 @@
 package com.github.lsj8367.tobyspringboot.config.autoconfig;
 
 import com.github.lsj8367.tobyspringboot.config.MyAutoConfiguration;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
 @MyAutoConfiguration
-public class TomcatWebServerConfig {
+public class JettyWebServerConfig {
 
-    @Bean("tomcatWebServerFactory")
+    @Bean("jettyWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+        return new JettyServletWebServerFactory();
     }
-
 }
