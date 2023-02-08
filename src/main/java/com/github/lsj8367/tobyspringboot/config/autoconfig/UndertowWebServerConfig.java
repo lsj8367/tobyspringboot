d7979e3b550abb106edce8cb817451b7a5d1143c
lsj8367 <lsj8367@naver.com>
2023-02-08 22:05:21 +0900

    :sparkles: conditionOnMissingBean 구현

--- src/main/java/com/github/lsj8367/tobyspringboot/config/autoconfig/UndertowWebServerConfig.java
+++ src/main/java/com/github/lsj8367/tobyspringboot/config/autoconfig/UndertowWebServerConfig.java
@@ -2,15 +2,18 @@ package com.github.lsj8367.tobyspringboot.config.autoconfig;
 
 import com.github.lsj8367.tobyspringboot.config.ConditionalMyOnClass;
 import com.github.lsj8367.tobyspringboot.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 
 @MyAutoConfiguration
 @ConditionalMyOnClass("io.undertow.Undertow")
 public class UndertowWebServerConfig {
 
-    @Bean("jettyWebServerFactory")
+    @Bean("undertowWebServerFactory")
+    @ConditionalOnMissingBean // 조건에 맞는게 있다면 이 빈 생성을 해주지 않는다.
     public ServletWebServerFactory servletWebServerFactory() {
         return new UndertowServletWebServerFactory();
     }
